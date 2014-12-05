1f8964ffa108c81062ce6bca3e1dccb81dd33f2a
Constantine <constantine.hatzis@gmail.com>
2014-12-05 14:23:46 -0800

    Added decimal: hexidecimal dictionary

--- Students/ConstantineHatzis/dict_set.py
+++ Students/ConstantineHatzis/dict_set.py
@@ -12,3 +12,8 @@ print(lab.keys())
 print(lab.values())
 print(u"cake" in lab)
 print(u"Mango" in lab.values())
+
+dec_rep = range(1, 16)
+hex_rep = u"123456789ABCDEF"
+dec_hex = dict(zip(dec_rep, hex_rep))
+print(dec_hex)
