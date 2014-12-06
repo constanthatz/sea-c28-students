460807577eeb3ddcaaf858d2a86aa7582ee1bdda
Constantine <constantine.hatzis@gmail.com>
2014-12-06 15:14:41 -0800

    Added code for stripping text of punctuation
    Using str.translate, string.punctuation, and a special dict table generated for unicode strings, I have stripped the punctuation from the imported text so that when I am searching for words for the trigrams I don't have to deal with the punctuation.

--- Students/ConstantineHatzis/trigrams.py
+++ Students/ConstantineHatzis/trigrams.py
@@ -1,5 +1,17 @@
 from __future__ import print_function
-import io
+import io  # For file operations
+import string  # For string.punctuation
 
 f = io.open('sherlock.txt', encoding='utf-8')
 text = f.read()
+
+# Strip punctuation from imported text
+strip_table = {}  # Initialize translation table
+for i in range(len(string.punctuation)):
+    # Generate translation table for unicode to turn puncuation into None.
+    # For unicode it is necessary to create a dict of keys of the ord() of the
+    # unicode character you want to replace with the associated value.
+    # Python 3 where are you
+    strip_table.update({ord(string.punctuation[i]): None})
+
+stripped_text = text.translate(strip_table)
