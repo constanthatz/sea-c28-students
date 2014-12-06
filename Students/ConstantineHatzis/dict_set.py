4ec570152ab27a667e5efbb0749fe3faab45af60
Constantine <constantine.hatzis@gmail.com>
2014-12-05 16:03:36 -0800

    Finished dict/set lab
    Add comments to the whole code, change a symbol to fix line lengths, and union and intersection task

--- Students/ConstantineHatzis/dict_set.py
+++ Students/ConstantineHatzis/dict_set.py
@@ -1,27 +1,34 @@
 from __future__ import print_function
 
-lab = {u"name": u"Chris", u"city": u"Seattle", u"cake": u"Chocolate"}
-print(lab)
+# Create dictionary
+ds = {u"name": u"Chris", u"city": u"Seattle", u"cake": u"Chocolate"}
+print(ds)
 
-del lab[u"cake"]
-print(lab)
+# Delete key from dictionary
+del ds[u"cake"]
+print(ds)
 
-lab.update({u"fruit": u"Mango"})
+# Add key: value to dictionary
+ds.update({u"fruit": u"Mango"})
 
-print(lab.keys())
-print(lab.values())
-print(u"cake" in lab)
-print(u"Mango" in lab.values())
+# Print elements of dictionary
+print(ds.keys())
+print(ds.values())
+print(u"cake" in ds)
+print(u"Mango" in ds.values())
 
+# Create dictionary of decimal: hexidecimal values
 dec_rep = range(1, 16)
 hex_rep = u"123456789ABCDEF"
 dec_hex = dict(zip(dec_rep, hex_rep))
 print(dec_hex)
 
-print(lab)
-lab_a = dict(zip(lab.keys(), [x.count(u"a") for x in map(lab.get, lab.keys())]))
-print(lab_a)
+# Create dictionary of original keys and original value 'a' counts
+print(ds)
+ds_a = dict(zip(ds.keys(), [x.count(u"a") for x in map(ds.get, ds.keys())]))
+print(ds_a)
 
+# Create sets of various divisibilities
 s1 = range(21)
 s2 = set([x for x in s1 if x % 2 == 0])
 s3 = set([x for x in s1 if x % 3 == 0])
@@ -31,5 +38,15 @@ print(s2)
 print(s3)
 print(s4)
 
+# Check if sets are subsets
 print(s3.issubset(s2))
 print(s4.issubset(s2))
+
+# Create string set, add string to set, create string frozenset
+python_set = set(list(u"Python"))
+python_set.add(u"i")
+marathon_set = frozenset(u"marathon")
+
+# Display union and intersection of sets
+print(python_set.union(marathon_set))
+print(python_set.intersection(marathon_set))
