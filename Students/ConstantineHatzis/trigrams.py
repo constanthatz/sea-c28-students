7428f86ff543538010c2245b6680c9109d7e471c
Constantine <constantine.hatzis@gmail.com>
2014-12-06 16:59:48 -0800

    Added dictionary creation code.
    Using a for loop and dict.setdefault, and list.append I created a dictionary of three word combinations from the imported text.

--- Students/ConstantineHatzis/trigrams.py
+++ Students/ConstantineHatzis/trigrams.py
@@ -6,7 +6,24 @@ f = io.open('sherlock_small.txt', encoding='utf-8')
 text = f.read()
 f.close()
 
+
 # Strip punctuation and newline characters from imported text to make creating
 # dictionary of word combinations easier
 split_text = text.split()
 stripped_text = [i.strip(string.punctuation) for i in split_text]
+
+# Generate dictionary of every
+# ["first_word", "second_word": ["third_word_1", "third_word_2", ... ]
+# combination in imported text
+
+word_dict = {}  # Initialize word dictionary
+for i in range(len(stripped_text)-2):
+    # For loop should only iterate until the third to last word
+    first_word = stripped_text[i]
+    second_word = stripped_text[i+1]
+    third_word = stripped_text[i+2]
+    word_dict.setdefault((first_word, second_word), []).append(third_word)
+
+
+if __name__ == "__main__":
+    text = "I wish I may I wish I might"
