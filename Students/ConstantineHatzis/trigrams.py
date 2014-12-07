6917d47d84912984df42b8731b2d6111960d1f2b
Constantine <constantine.hatzis@gmail.com>
2014-12-07 13:45:07 -0800

    Added code for find next random word and appending it the story.
    The story will continue until a next word can't be found and it will end.

--- Students/ConstantineHatzis/trigrams.py
+++ Students/ConstantineHatzis/trigrams.py
@@ -30,56 +30,30 @@ for i in range(len(stripped_text)-2):
     word_dict.setdefault((first_word, second_word), []).append(third_word)
 
 
-def ask_for_words():
-    u"""Return the words that the user inputed plus a random third word from
-        the dictionary."""
-    try:
-        prompt = u"""Type one word, two words, a punctuation mark (.?!,), or \
-FIN to end your story: """
-        words = raw_input(prompt)
-    except (EOFError, KeyboardInterrupt):
-        return u" FIN"
-    else:
-        u_words = unicode(words)
-
-        if u_words == u"FIN":
-            trigram = u" " + u_words
-        elif u_words == u"." or u_words == u"?" or u_words == u"!" or \
-                u_words == u"," or u_words == u"\t":
-            trigram = u_words
-        elif u_words == "":
-            trigram = u"\n"
-        else:
-            list_u_words = u_words.split()
-
-            if len(list_u_words) == 1:
-                trigram = u" " + u" ".join(list_u_words)
-            elif len(list_u_words) != 2:
-                print(u"That was not {} or {} words, try again: ".format(1, 2))
-                trigram = ask_for_words()
-            else:
-                look_up_words = tuple([x.lower() for x in list_u_words])
-                if word_dict.get(look_up_words):
-                    next_word = random.choice(word_dict.get(look_up_words))
-                    list_u_words.append(next_word)
-                    trigram = u" " + u" ".join(list_u_words)
-                else:
-                    print(u"You chose poorly, try again: ")
-                    trigram = ask_for_words()
-    return trigram
+story = ()
 
 
-def write_story(story):
-    trigram = u""
-    while trigram != u" FIN":
-        trigram = ask_for_words()
-        story += trigram
-        print(story)
+def write_story(story, word_dict):
+    pair = random.choice(word_dict.keys())  # Get starting point for story
+    story += pair
+
+    third = u"1"
+    while third:
+        third = find_next_word(pair)
+        print(third)
+        story += (third,)
+        pair = story[-2:]
+
     return story
 
-story = ""
-story = write_story(story)
-print(story)
+
+def find_next_word(pair):
+    third = random.choice(word_dict.get(pair))
+    return third
+
+story = write_story(story, word_dict)
+print(" ".join(story))
+
 
 if __name__ == "__main__":
     text = "I wish I may I wish I might"
