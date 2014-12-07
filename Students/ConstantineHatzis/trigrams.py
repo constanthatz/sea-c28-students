57588f0ec84c197f43b5c2f48235e9708ae659b4
Constantine <constantine.hatzis@gmail.com>
2014-12-06 16:28:41 -0800

    Fixed code for stripping puntuation and added newline character stripping.
    The first version of the code didn't remove newline characters automatically and removed punctuation from within words."

--- Students/ConstantineHatzis/trigrams.py
+++ Students/ConstantineHatzis/trigrams.py
@@ -2,16 +2,11 @@ from __future__ import print_function
 import io  # For file operations
 import string  # For string.punctuation
 
-f = io.open('sherlock.txt', encoding='utf-8')
+f = io.open('sherlock_small.txt', encoding='utf-8')
 text = f.read()
+f.close()
 
-# Strip punctuation from imported text
-strip_table = {}  # Initialize translation table
-for i in range(len(string.punctuation)):
-    # Generate translation table for unicode to turn puncuation into None.
-    # For unicode it is necessary to create a dict of keys of the ord() of the
-    # unicode character you want to replace with the associated value.
-    # Python 3 where are you
-    strip_table.update({ord(string.punctuation[i]): None})
-
-stripped_text = text.translate(strip_table)
+# Strip punctuation and newline characters from imported text to make creating
+# dictionary of word combinations easier
+split_text = text.split()
+stripped_text = [i.strip(string.punctuation) for i in split_text]
