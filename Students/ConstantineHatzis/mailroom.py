392ed60f0e3b013b0610b6747da24b18e17a2eba
Constantine <constantine.hatzis@gmail.com>
2014-12-08 12:27:13 -0800

    Added docstrings and safe_input().
    Add docstrings to functions and started replacing raw_input with smarter safe_input.

--- Students/ConstantineHatzis/mailroom.py
+++ Students/ConstantineHatzis/mailroom.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from __future__ import division
-import io
 
 # Data structure for donors and donations using dictionary
 donors = {}
@@ -13,34 +12,42 @@ donors[u"Beric Dondarrion"] = [1320.75, 5020.25]
 donors[u"Marie Curie"] = [1000, 3200.50]
 
 
-def first_level_prompt():
-    """ Ask the user if they want to quit, send a thank you, create report """
-    # Ask user what they want to do
-
+def safe_input(prompt):
+    """Return user input after catching KeyboardInterrupt and EOFError"""
     try:
-        prompt = u"\n1: Send a Thank You \n2: Create a Report \nq: Quit \n?: "
         reply = raw_input(prompt)
     except (EOFError, KeyboardInterrupt):
-        return None
+        return quit()
     else:
-        reply = unicode(reply)  # Convert input to unicode
-
-        if reply.lower() == u"q":
-            quit()
-        elif reply == u"1":
-            reply = thank_you_prompt()
-        elif reply == u"2":
-            reply = create_report()
-        else:
-            print(u"\nNot a valid choice, please choose again.")
-            reply = first_level_prompt()
         return reply
 
 
+def first_level_prompt():
+    """Return user input. Quit, send thank you email to donor, or create report
+        or donors."""
+
+    # Ask user what they want to do
+    prompt = u"\n1: Send a Thank You \n2: Create a Report \nq: Quit \n?: "
+    reply = safe_input(prompt)
+
+    reply = unicode(reply)  # Convert input to unicode
+
+    if reply.lower() == u"q":
+        quit()
+    elif reply == u"1":
+        reply = thank_you_prompt()
+    elif reply == u"2":
+        reply = create_report()
+    else:
+        print(u"\nNot a valid choice, please choose again.")
+        reply = first_level_prompt()
+    return reply
+
+
 def thank_you_prompt():
-    """ Prompt user to see the list of donors, or for a name of a donor, their
-        donation create a thank you note. If it is a new donor, add the name to
-        the list of donors. """
+    """Return user input. Prompt user (1) to see list of donors, (2) for donor
+        name and their donation amount and create a thank you note. If it is a
+        new donor, add their name to the list of donors."""
 
     try:
         prompt = u"\nFull Name (q: quit, s: start over): "
@@ -67,7 +74,7 @@ def thank_you_prompt():
 
 
 def name_donation(name):
-    """ Update donors dict with new donation and name if necessary"""
+    """Return name of donor and their donation amount."""
     donors.setdefault(name, [])
     donation = new_donation()
     donors.get(name).append(donation)
@@ -75,7 +82,7 @@ def name_donation(name):
 
 
 def new_donation():
-    """ Update donors dict with new donor """
+    """Return donation amount."""
     try:
         prompt = u"\nDonation Amount (q: quit, s: start over): "
         reply = raw_input(prompt)
@@ -101,29 +108,35 @@ def new_donation():
 
 
 def compose_thank_you(name, donation):
-    """ Compose thank you email, print it, and return to original prompt """
+    """Print thank you email from template"""
 
-    print(u"\n\t{}, thank you for your generous donation of ${:.2f}.".format(name, donation))
+    template = u"\n\t{}, thank you for your generous donation of${:.2f}." \
+        .format(name, donation)
+    print(template)
     reply = first_level_prompt()
     return reply
 
 
 def create_report():
-    """Return a report of all donors, average donation amoutn, number of donations, and total donations"""
+    """Return a report of all donors, average donation amount, number of
+    donations, and total donations"""
     report_matrix = {}
     for x in donors:
         donations = donors[x]
         total_donations = sum(donations)
         number_donations = len(donations)
-        average_donation = total_donations / number_donations
-        report_matrix[x] = [total_donations, number_donations, average_donation]
+        avg_donation = total_donations / number_donations
+        report_matrix[x] = [total_donations, number_donations, avg_donation]
 
-    header = [u"Donor", u"Total Donations", u"Number of Donations", u"Average Donations"]
+    header = [u"Donor", u"Total Donations", u"Number of Donations",
+              u"Average Donations"]
 
     print(u"{0:<20}{1:>20}{2:>30}{3:>30}\n".format(*header))
-
     for x in report_matrix:
-        print(u"{0:<20}{1:>20}{2:>30}{3:>30}\n".format(x, "${:.2f}".format(report_matrix[x][0]), report_matrix[x][1], "${:.2f}".format(report_matrix[x][2])))
+        args = [x, "${:.2f}".format(report_matrix[x][0]), report_matrix[x][1],
+                "${:.2f}".format(report_matrix[x][2])]
+        print(u"{0:<20}{1:>20}{2:>30}{3:>30}\n".format(*args))
+
     reply = first_level_prompt()
     return reply
 
