b5024c2cd447eff8684c0b9b70c2b413c28cc6ac
Constantine <constantine.hatzis@gmail.com>
2014-12-08 13:35:37 -0800

    Added docstring.

--- Students/ConstantineHatzis/safe_input.py
+++ Students/ConstantineHatzis/safe_input.py
@@ -1,4 +1,6 @@
 def safe_input(prompt="What?: "):
+    """Return user imput or exiting program when KeyboardInterrupt or EOFError
+    is caught"""
     try:
         input = raw_input(prompt)
     except (EOFError, KeyboardInterrupt):
