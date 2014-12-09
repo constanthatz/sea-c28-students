4a870e84526e86975dd395c4e4b90a9ccd9bd019
Constantine <constantine.hatzis@gmail.com>
2014-12-08 16:25:41 -0800

    I think someof my commits were never made, so I will combine them here.
    I added random punctuation and paragraph generation. It can be controlled by how many words you want to consider a sentence or a paragraph. I added more code to strip punctuation and numbers from the original code, because it was interfering with how I was picking the pairs for the trigrams.

--- Students/ConstantineHatzis/trigrams.py
+++ Students/ConstantineHatzis/trigrams.py
@@ -12,13 +12,21 @@ f = io.open('sherlock.txt', encoding='utf-8')
 text = f.readlines()[52:12685]
 f.close()
 # Join all the lines of text into one text string
-text = " ".join(text)
+text = u" ".join(text)
 
 
 # Strip punctuation and newline characters from imported text to make creating
 # dictionary of word combinations easier.
 split_text = text.split()
 stripped_text = [i.strip(string.punctuation) for i in split_text]
+
+# Remove words with numbers or punctuation inside of them
+# This makes adding random punctuation easier later.
+# These words will not be caught by str.strip
+for x in stripped_text[:]:
+    if not x.isalpha():
+        stripped_text.remove(x)
+
 
 # Generate dictionary of every
 # ["first_word", "second_word": ["third_word_1", "third_word_2", ... ]
@@ -40,22 +48,42 @@ def write_story(story, word_dict):
 
     # The while will run until a next word can't be found, and the story ends.
     third = u"1"
-    while third:
+    i = 0
+    while third and len(story) < 5000:
+        i += 3  # Count words
         third = find_next_word(pair)  # Find the next word
         story += (third,)  # Append the next word to the story
-        pair = story[-2:]  # Select the last two words to get the next one
+
+        # Insert random sentence ender or comma after either 9, 12, or 15 words
+        if i % random.choice([27, 81]) == 0:
+            punctuation = [u".", u"!", u"?", u",", ""]
+            story += (random.choice(punctuation),)
+
+        # Start new paragrap randomly after 300, 600, or 900 words
+        if i % random.choice([300, 600, 900]) == 0:
+            story += (u"\n\n",)
+
+        #  Select the next pair of words to be used to find the trigram, while
+        # not choosing the punctuation
+        j = 1
+        pair = ()
+        while len(pair) < 2:
+            if story[-j].isalnum():
+                pair = (story[-j],) + pair
+
+            j += 1
 
     story += (u"\n\nFIN.",)
     return story
 
 
 def find_next_word(pair):
-    """Return the next woprd based on two words using an imported text"""
+    """Return the next word based on two words using an imported text"""
     third = random.choice(word_dict.get(pair))
     return third
 
 if __name__ == "__main__":
-    story = ()
+    story = (u"A", u"Sherlock", u"Holmes", u"Mystery")
     story = write_story(story, word_dict)
     print(" ".join(story))
-    print(u"\n This story is {} words long.".format(len(story)))
+    print(u"\nThis story is {} words long.".format(len(story)))
