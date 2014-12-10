ea174a8b8255a2d513c869344632e26ca33c5d5e
Constantine Hatzis <constantine.hatzis@gmail.com>
2014-12-10 00:17:05 -0800

    Add if name main block.

--- Students/ConstantineHatzis/count_evens.py
+++ Students/ConstantineHatzis/count_evens.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 def count_evens(nums):
     return len([x for x in nums if not x % 2])
 
-nums = range(100)
-num_evens = count_evens(nums)
-print(num_evens)
+
+if __name__ == "__main__":
+    nums = range(100)
+    num_evens = count_evens(nums)
+    print(num_evens)
