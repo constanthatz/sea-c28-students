f2935acac51914c3f3558b9ce28a882a2b835dcb
Constantine Hatzis <constantine.hatzis@gmail.com>
2014-12-10 01:12:49 -0800

    Fixed function_builder() and added if name main block.
    Fixed funtion builder with keyword arguments and printed out test cases.

--- Students/ConstantineHatzis/lambda_magic.py
+++ Students/ConstantineHatzis/lambda_magic.py
@@ -2,6 +2,11 @@ from __future__ import print_function
 
 
 def function_builder(n):
-    the_list = []
+    the_list = [lambda x, y=i: x + y for i in range(n)]
+    return the_list
+
+if __name__ == "__main__":
+    n = 4
+    the_list = function_builder(4)
     for i in range(n):
-        the_list += [lambda x: n+x]
+        print([the_list[i](j) for j in range(10)])
