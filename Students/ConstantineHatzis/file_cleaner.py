5a16b995e87c3a42b36859803d641fec29b2793e
Constantine Hatzis <constantine.hatzis@gmail.com>
2014-12-10 11:05:40 -0800

    Add file open, read, and close operations.

--- Students/ConstantineHatzis/file_cleaner.py
+++ Students/ConstantineHatzis/file_cleaner.py
@@ -1,1 +1,11 @@
 from __future__ import print_function
+import sys
+import io
+
+
+filename = sys.argv[1]
+f = io.open(filename, mode="+", encoding='utf-8')
+text = f.readlines()
+f.close()
+
+print(text)
