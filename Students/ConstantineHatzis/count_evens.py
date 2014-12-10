73ee0da4bd96a21e5a673443abdb80adebb3e551
Constantine Hatzis <constantine.hatzis@gmail.com>
2014-12-10 10:40:21 -0800

    Added docstring.

--- Students/ConstantineHatzis/count_evens.py
+++ Students/ConstantineHatzis/count_evens.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 
 def count_evens(nums):
+    """Return the number of even numbers in a list of numbers"""
     return len([x for x in nums if not x % 2])
 
 
