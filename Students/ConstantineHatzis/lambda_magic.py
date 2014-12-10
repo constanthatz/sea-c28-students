90480f5825c4502e354364a445bea641e873ab78
Constantine Hatzis <constantine.hatzis@gmail.com>
2014-12-10 10:37:30 -0800

    Added comments.

--- Students/ConstantineHatzis/lambda_magic.py
+++ Students/ConstantineHatzis/lambda_magic.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+# Generate list of functions
+
 
 def function_builder(n):
     return [lambda x, y=i: x + y for i in range(n)]
@@ -7,5 +9,7 @@ def function_builder(n):
 if __name__ == "__main__":
     n = 10
     the_list = function_builder(n)
+
+    # Test cases for the functions in the_list
     for f in the_list:
         print([f(j) for j in range(10)])
