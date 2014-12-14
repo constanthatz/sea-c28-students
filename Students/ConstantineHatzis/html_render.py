55096d511f490fb19e773579c0b1e19b2ddacc90
Constantine <constantine.hatzis@gmail.com>
2014-12-14 14:06:19 -0800

    Step 1 mostly complete.

--- Students/ConstantineHatzis/html_render.py
+++ Students/ConstantineHatzis/html_render.py
@@ -2,12 +2,16 @@
 
 
 class Element(object):  # change list to object and solve the problem
-    opening_tag = "<>"
-    closing_tag = "</>"
+    opening_tag = u"<>"
+    closing_tag = u"</>"
+    indent = ""
 
     def __init__(self):
         self.contents = []
 
-    def render(self):
+    def append(self,string):
+    	self.contents.append(string)
+
+    def render(self,file_out, ind = ""):
         all_out = [self.opening_tag] + self.contents + [self.closing_tag]
-        print "\n".join(all_out)
+     	file_out.write("\n".join(all_out))
