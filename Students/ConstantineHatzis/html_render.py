ad346850e502c870e5eb783ce9f89681c7ae4f8b
Constantine <constantine.hatzis@gmail.com>
2014-12-14 16:09:41 -0800

    Added handling of strings or elements with render method, to append method.

--- Students/ConstantineHatzis/html_render.py
+++ Students/ConstantineHatzis/html_render.py
@@ -1,34 +1,49 @@
 #!/usr/bin/env python
 
+import cStringIO
+
 
 class Element(object):  # change list to object and solve the problem
-    opening_tag = u"<>"
-    closing_tag = u"</>"
+    opening_tag = [u"<>"]
+    closing_tag = [u"</>"]
     indent = ""
 
     def __init__(self, contents=None):
-        self.contents = []
+        if contents:
+            self.contents = [contents]
+        else:
+            self.contents = []
 
     def append(self, string):
-        self.contents.append(string)
+        try:
+            f = cStringIO.StringIO()
+            string.render(f)
+        except AttributeError:
+            self.contents += [string]
+        else:
+            f.reset()
+            self.contents += [f.read()]
+            print self.contents
+            f.close()
 
     def render(self, file_out, ind=""):
-        all_out = [self.opening_tag] + \
+        all_out = self.opening_tag + \
             [u"{:4}".format(u"") + x for x in self.contents] + \
-            [self.closing_tag]
+            self.closing_tag
+        # print all_out
         file_out.write("\n".join(all_out))
 
 
 class Html(Element):
-    opening_tag = u"<html>"
-    closing_tag = u"</html>"
+    opening_tag = [u"<html>"]
+    closing_tag = [u"</html>"]
 
 
 class Body(Element):
-    opening_tag = u"<body>"
-    closing_tag = u"</body>"
+    opening_tag = [u"<body>"]
+    closing_tag = [u"</body>"]
 
 
 class P(Element):
-    opening_tag = u"<p>"
-    closing_tag = u"</p>"
+    opening_tag = [u"<p>"]
+    closing_tag = [u"</p>"]
