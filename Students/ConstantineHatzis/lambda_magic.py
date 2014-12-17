39bde070a43e9896a1b08a870a504dbf35470f8f
Constantine <constantine.hatzis@gmail.com>
2014-12-16 18:04:01 -0800

    Created test files for ack.py, rot13.py, series.py, and lambda_magic.py.

---
+++ Students/ConstantineHatzis/lambda_magic.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+# Generate list of functions
+
+
+def function_builder(n):
+    """Return a list of sequential functions."""
+    return [lambda x, y=i: x + y for i in range(n)]
+
+if __name__ == "__main__":
+    n = 10
+    the_list = function_builder(n)
+
+    # Test cases for the functions in the_list
+    for f in the_list:
+        print([f(j) for j in range(10)])
