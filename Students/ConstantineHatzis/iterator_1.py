d1ceb6ccfd8c6104b033de7c0f4ebf8c28931417
Constantine <constantine.hatzis@gmail.com>
2014-12-18 01:24:35 -0800

    Modified IterateMe_2 to accept a start, stop and step value.

---
+++ Students/ConstantineHatzis/iterator_1.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+"""
+Simple iterator examples
+"""
+
+
+class IterateMe_1(object):
+    """
+    About as simple an iterator as you can get:
+
+    returns the sequence of numbers from zero to 4
+    ( like xrange(4) )
+    """
+
+    def __init__(self, stop=5):
+        self.current = -1
+        self.stop = stop
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        self.current += 1
+        if self.current < self.stop:
+            return self.current
+        else:
+            raise StopIteration
+
+
+class IterateMe_2(object):
+    """
+    About as simple an iterator as you can get:
+
+    returns the sequence of numbers from zero to 4
+    ( like xrange(4) )
+    """
+    def __init__(self, start=0, stop=10, step=1):
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.current = self.start-self.step
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        self.current += self.step
+        if self.current < self.stop:
+            return self.current
+        else:
+            raise StopIteration
+
+
+if __name__ == "__main__":
+
+    print "first version"
+    for i in IterateMe_1():
+        print i
+
+    print "second version"
+    for i in IterateMe_2(4,10):
+        print i
