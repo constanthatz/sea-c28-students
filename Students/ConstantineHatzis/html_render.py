93756d33ea35920ad10b1a644c96873644a13ded
Constantine <constantine.hatzis@gmail.com>
2014-12-18 19:43:26 -0800

    Fixed selfclosingtag to reference tags properly.

--- Students/ConstantineHatzis/html_render.py
+++ Students/ConstantineHatzis/html_render.py
@@ -104,14 +104,14 @@ class SelfClosingTag(Element):
     def render(self, file_out, ind=indent):
         temp_tags = list(self.tags)
         if self.kwargs:
-            list_tag = list(temp_tags[0])
+            list_tag = temp_tags
 
             for key in self.kwargs:
                 list_tag.insert(-2, u'{}="{}"'.format(key, self.kwargs[key]))
-            temp_tags[0] = "".join(list_tag)
+            temp_tags = "".join(list_tag)
 
         # Combine the single tag into a list.
-        all_out = [temp_tags[0]]
+        all_out = temp_tags
         file_out.write("".join(all_out))
 
 
