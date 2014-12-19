abb5deecc336b7e6a212d4614d1374d884ea54e8
Constantine <constantine.hatzis@gmail.com>
2014-12-18 22:25:41 -0800

    Increased default limits in intsum() and doubler().

--- Students/ConstantineHatzis/generator_solution.py
+++ Students/ConstantineHatzis/generator_solution.py
@@ -1,4 +1,4 @@
-def intsum(start=0, stop=100):
+def intsum(start=0, stop=2**100):
     i = start
     j = start
     while i < stop:
@@ -7,7 +7,7 @@ def intsum(start=0, stop=100):
         j += i
 
 
-def doubler(start=1, stop=100):
+def doubler(start=1, stop=2**100):
     i = start
     while i < stop:
         yield i
