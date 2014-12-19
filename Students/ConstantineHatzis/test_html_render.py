f8a275a2d36d39cab6a98d29fc9f96cd1b8e7e52
Constantine <constantine.hatzis@gmail.com>
2014-12-18 20:04:42 -0800

    Added Html() unittest.

--- Students/ConstantineHatzis/test_html_render.py
+++ Students/ConstantineHatzis/test_html_render.py
@@ -1,9 +1,17 @@
 import unittest
+import cStringIO
 import html_render as hr
 
-class MyTests(unittest.Testcase):
-    def test_Html
-        
+class MyTests(unittest.TestCase):
+    def test_Html(self):
+        expected = '<!DOCTYPE html>\n<html>\n</html>'
+
+        page = hr.Html()
+        f = cStringIO.StringIO()
+        page.render(f)
+        f.reset()
+        actual = f.read()
+        self.assertEquals(expected, actual)
 
 if __name__ == '__main__':
     unittest.main()
