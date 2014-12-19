048b0782e72d68d44bdf18cf53dce309297e43fe
Constantine <constantine.hatzis@gmail.com>
2014-12-18 22:21:17 -0800

    Added doubler() generator.

--- Students/ConstantineHatzis/generator_solution.py
+++ Students/ConstantineHatzis/generator_solution.py
@@ -5,3 +5,10 @@ def intsum(start=0, stop=100):
         yield j
         i += 1
         j += i
+
+
+def doubler(start=1, stop=100):
+    i = start
+    while i < stop:
+        yield i
+        i *= 2
