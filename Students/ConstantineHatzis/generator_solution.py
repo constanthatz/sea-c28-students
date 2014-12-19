36cea10d3163dc8dcee268258c892cdc2b30c990
Constantine <constantine.hatzis@gmail.com>
2014-12-18 22:33:08 -0800

    Add fib() genertator and cleaned up symbol assignment.

--- Students/ConstantineHatzis/generator_solution.py
+++ Students/ConstantineHatzis/generator_solution.py
@@ -1,6 +1,5 @@
 def intsum(start=0, stop=2**100):
-    i = start
-    j = start
+    i, j = start, start
     while i < stop:
         yield j
         i += 1
@@ -12,3 +11,10 @@ def doubler(start=1, stop=2**100):
     while i < stop:
         yield i
         i *= 2
+
+
+def fib(start=0, stop=2**100):
+    i, j = start, start + 1
+    while j < stop:
+        yield j
+        i, j = j, i + j
