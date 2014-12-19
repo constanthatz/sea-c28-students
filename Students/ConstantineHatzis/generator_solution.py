23659635460c48a33ef4c062100ad900b5b6ea5f
Constantine <constantine.hatzis@gmail.com>
2014-12-18 23:22:59 -0800

    Added prime() generator based on a Python implementation of Wilson's Theorem found on Wikipedia.

--- Students/ConstantineHatzis/generator_solution.py
+++ Students/ConstantineHatzis/generator_solution.py
@@ -18,3 +18,21 @@ def fib(start=0, stop=2**100):
     while j < stop:
         yield j
         i, j = j, i + j
+
+
+def prime(start=1, stop=2**100):
+    def is_prime(n):
+        if n <= 3:
+            return n >= 2
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        for i in range(5, int(n ** 0.5) + 1, 6):
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+        return True
+
+    n = start
+    while n < stop:
+        if is_prime(n):
+            yield n
+        n += 1
